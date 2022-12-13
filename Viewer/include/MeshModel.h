f19176d079eb6be795b15c5bb262a579fecea0e4
YusraRa <yusra.rayyan22@gmail.com>
2022-12-13 18:28:54 +0200

    Update MeshModel.h

--- Viewer/include/MeshModel.h
+++ Viewer/include/MeshModel.h
@@ -23,22 +23,37 @@ public:
 	void SetworldTransform();
 
 	glm::mat4 GetOBJScale();
-	void SetOBJScale(float s);
+	void SetOBJScale(float s, int index);
 
 	glm::mat4 GetOBJTranslate();
-	void SetOBJTranslate(float t);
+	void SetOBJTranslate(float t, int index);
 
-	glm::mat4 GetOBJRotate();
-	void SetOBJRotate(float degree);
+	glm::mat4 GetOBJRotate_X();
+	void SetOBJRotate_X(float degree);
+
+	glm::mat4 GetOBJRotate_Y();
+	void SetOBJRotate_Y(float degree);
+
+	glm::mat4 GetOBJRotate_Z();
+	void SetOBJRotate_Z(float degree);
 
 	glm::mat4 GetWORLDScale();
-	void SetWORLDScale(float s);
+	void SetWORLDScale(float s, int index);
 
 	glm::mat4 GetWORLDTranslate();
-	void SetWORLDTranslate(float t);
+	void SetWORLDTranslate(float t,int index);
 
-	glm::mat4 GetWORLDRotate();
-	void SetWORLDRotate(float degree);
+	glm::mat4 GetWORLDRotate_X();
+	void SetWORLDRotate_X(float degree);
+
+	glm::mat4 GetWORLDRotate_Y();
+	void SetWORLDRotate_Y(float degree);
+
+	glm::mat4 GetWORLDRotate_Z();
+	void SetWORLDRotate_Z(float degree);
+
+	glm::mat4 Mul_RotateMat_World();
+	glm::mat4 Mul_RotateMat_OBJ();
 
 
 private:
@@ -46,12 +61,18 @@ private:
 	std::vector<glm::vec3> vertices;
 	std::vector<glm::vec3> normals;
 	std::string model_name;
+
 	glm::mat4 objectTransform;
 	glm::mat4 Scale_obj;
 	glm::mat4 Translate_obj;
-	glm::mat4 Rotate_obj;
+	glm::mat4 Rotate_obj_x;
+	glm::mat4 Rotate_obj_y;
+	glm::mat4 Rotate_obj_z;
+
+	glm::mat4 worldTransform;
 	glm::mat4 Scale_world;
 	glm::mat4 Translate_world;
-	glm::mat4 Rotate_world;
-	glm::mat4 worldTransform;
+	glm::mat4 Rotate_world_x;
+	glm::mat4 Rotate_world_y;
+	glm::mat4 Rotate_world_z;
 };
