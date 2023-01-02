56a112894ec4fcd8323d8e44f500ffa4769e122a
YusraRa <yusra.rayyan22@gmail.com>
2023-01-02 15:57:54 +0200

    Update Renderer.h

--- Viewer/include/Renderer.h
+++ Viewer/include/Renderer.h
@@ -8,7 +8,7 @@ class Renderer
 public:
 	Renderer(int viewportWidth, int viewportHeight);
 	virtual ~Renderer();
-	void Render(const Scene& scene);
+	void Render( Scene& scene);
 	void SwapBuffers();
 	void ClearColorBuffer(const glm::vec3& color);
 	int GetViewportWidth() const;
@@ -20,6 +20,8 @@ public:
 	void FixPoints(MeshModel& Mesh);
 	void worldMat(MeshModel& Mesh);
 	void Transformation(MeshModel& Mesh);
+	void DrawBoundingBox(MeshModel& Mesh);
+
 	//void udateModelTranform(glm::mat4x4 Transform, MeshModel& Mesh);
 
 	
