6505cd326f327b125de0bbf770932f5fb00859ed
YusraRa <yusra.rayyan22@gmail.com>
2023-01-05 19:55:55 +0200

    Update MeshModel.cpp
    1, 2 ,3 , half 7, 8

--- Viewer/src/MeshModel.cpp
+++ Viewer/src/MeshModel.cpp
@@ -67,9 +67,7 @@ MeshModel::MeshModel(std::vector<Face> faces, std::vector<glm::vec3> vertices, s
 	{ 0.0f, 1.0f, 0.0f, 0.0f },
 	{ 0.0f, 0.0f, 1.0f, 0.0f },
 	{ 0.0f, 0.0f, 0.0f, 1.0f } };
-
-	glm::fvec3 max = glm::fvec3(vertices[0].x, vertices[0].y, vertices[0].z);
-	glm::fvec3 min = glm::fvec3(vertices[0].x, vertices[0].y, vertices[0].z);
+;
 	FindMax(); FindMin();
 }
 
@@ -95,7 +93,7 @@ glm::mat4 MeshModel::GetOBJTranslate()
 }
 void MeshModel::SetOBJTranslate(float t, int index)
 {
-	Translate_obj[index][2] = t; 
+	Translate_obj[index][3] = t; 
 }
 
 glm::mat4 MeshModel::GetOBJRotate_X()
@@ -137,25 +135,25 @@ glm::mat4 MeshModel::Mul_RotateMat_OBJ()
 	{ 0.0f, 1.0f, 0.0f, 0.0f },
 	{ 0.0f, 0.0f, 1.0f, 0.0f },
 	{ 0.0f, 0.0f, 0.0f, 1.0f } },
-		XYZ = { { 1.0f, 0.0f, 0.0f, 0.0f },
+	XYZ = { { 1.0f, 0.0f, 0.0f, 0.0f },
 	{ 0.0f, 1.0f, 0.0f, 0.0f },
 	{ 0.0f, 0.0f, 1.0f, 0.0f },
 	{ 0.0f, 0.0f, 0.0f, 1.0f } };;
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += Rotate_obj_x[i][k] * Rotate_obj_y[k][j];
 			}
 			XY[i][j] = sum;
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += XY[i][k] * Rotate_obj_z[k][j];
 			}
 			XYZ[i][j] = sum;
@@ -177,20 +175,20 @@ void MeshModel::SetobjectTransform()
 	{ 0.0f, 0.0f, 1.0f, 0.0f },
 	{ 0.0f, 0.0f, 0.0f, 1.0f } }, Rotate_obj = Mul_RotateMat_OBJ();
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += Scale_obj[i][k] * Rotate_obj[k][j];
 			}
 			SR[i][j] = sum;
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += SR[i][k] * Translate_obj[k][j];
 			}
 			objectTransform[i][j] = sum;
@@ -218,7 +216,7 @@ glm::mat4 MeshModel::GetWORLDTranslate()
 }
 void MeshModel::SetWORLDTranslate(float t,int index)
 {
-	Translate_world[index][2] = t;
+	Translate_world[index][3] = t;
 }
 
 glm::mat4 MeshModel::GetWORLDRotate_X()
@@ -260,25 +258,25 @@ glm::mat4 MeshModel::Mul_RotateMat_World()
 	{ 0.0f, 1.0f, 0.0f, 0.0f },
 	{ 0.0f, 0.0f, 1.0f, 0.0f },
 	{ 0.0f, 0.0f, 0.0f, 1.0f } },
-		XYZ = { { 1.0f, 0.0f, 0.0f, 0.0f },
+	XYZ = { { 1.0f, 0.0f, 0.0f, 0.0f },
 	{ 0.0f, 1.0f, 0.0f, 0.0f },
 	{ 0.0f, 0.0f, 1.0f, 0.0f },
 	{ 0.0f, 0.0f, 0.0f, 1.0f } };;
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += Rotate_world_x[i][k] * Rotate_world_y[k][j];
 			}
 			XY[i][j] = sum;
 		}
 	}
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += XY[i][k] * Rotate_world_z[k][j];
 			}
 			XYZ[i][j] = sum;
@@ -300,19 +298,19 @@ void MeshModel::SetworldTransform()
 	{ 0.0f, 1.0f, 0.0f, 0.0f },
 	{ 0.0f, 0.0f, 1.0f, 0.0f },
 	{ 0.0f, 0.0f, 0.0f, 1.0f } }, Rotate_world=Mul_RotateMat_World();
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += Scale_world[i][k] * Rotate_world[k][j];
 			}
 			SR[i][j] = sum;
 		}
 	}
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += SR[i][k] * Translate_world[k][j];
 			}
 			worldTransform[i][j] = sum;
@@ -325,10 +323,10 @@ void MeshModel::SetworldTransform()
 glm::mat4x4 matrix_mul(glm::mat4x4 m1, glm::mat4x4 m2)
 {
 	glm::mat4x4 ret;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += m1[i][k] * m2[k][j];
 			}
 			ret[i][j] = sum;
@@ -342,10 +340,10 @@ glm::mat4x4 MeshModel::GetTransformation()
 	SetobjectTransform();
 	SetworldTransform();
 	glm::mat4x4 trans;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			float sum = 0.0f;
-			for (int k = 0; k < 3; k++) {
+			for (int k = 0; k < 4; k++) {
 				sum += worldTransform[i][k] * objectTransform[k][j];
 			}
 			trans[i][j] = sum;
@@ -388,33 +386,39 @@ int MeshModel::RetVerticesSize()
 
 glm::fvec3 MeshModel::FindMin()
 {
+	float min_x = vertices[0].x, min_y = vertices[0].y, min_z = vertices[0].z;
 	for (int i = 0; i < vertices.size(); i++)
 	{
 		// min x
-		if (min.x > vertices[i].x)
+		if (min_x > vertices[i].x)
 			min.x = vertices[i].x;
 		// min y
-		if (min.y > vertices[i].y)
-			min.y = vertices[i].y;
+		if (min_y > vertices[i].y)
+			min_y = vertices[i].y;
 		// min z
-		if (min.z > vertices[i].z)
-			min.z = vertices[i].z;
+		if (min_z > vertices[i].z)
+			min_z = vertices[i].z;
 	}
+	min = glm::fvec3(min_x, min_y, min_z);
+	//std::cout << "min: " << min.x << "  " << min.y << "  " << min.z << "\n";
 	return min;
 }
 glm::fvec3 MeshModel::FindMax() {
+	float max_x = vertices[0].x, max_y = vertices[0].y, max_z = vertices[0].z;
 	for (int i = 0; i < vertices.size(); i++)
 	{
 		// max x
-		if (max.x < vertices[i].x)
-			max.x = vertices[i].x;
+		if (max_x < vertices[i].x)
+			max_x = vertices[i].x;
 		// max y
-		if (max.y < vertices[i].y)
-			max.y = vertices[i].y;
+		if (max_y < vertices[i].y)
+			max_y = vertices[i].y;
 		// max z
-		if (max.z < vertices[i].z)
-			max.z = vertices[i].z;
+		if (max_z < vertices[i].z)
+			max_z = vertices[i].z;
 	}
+	max = glm::fvec3(max_x, max_y, max_z);
+	//std::cout << "max: " << max.x << "  " << max.y << "  " << max.z << "\n";
 	return max;
 }
 
@@ -436,15 +440,28 @@ void MeshModel::SetMax(glm::fvec3 m)
 	max = m;
 }
 
-//void MeshModel::BoundingBox() {
-//	// find min max
-//	glm::fvec3 max = FindMax();
-//	glm::fvec3 min = FindMin();
-//	// size
-//	glm::fvec3 size = glm::vec3(max.x - min.x, max.y - min.y, max.z - min.z);
-//	// center
-//	glm::fvec3 center = glm::fvec3((max.x - min.x) / 2.0f, (max.y - min.y) / 2.0f, (max.z - min.z) / 2.0f);
-//	glm::mat4 transform = glm::translate(glm::mat4(1), center) * glm::scale(glm::mat4(1), size);
-//	// applying the object transforamaion of the box
-//	glm::mat4x4 m = GetTransformation() * transform;
-//}
+std::vector<glm::fvec3> MeshModel::BoundingBox() 
+{
+	std::vector<glm::fvec3> bb;
+	//bb.push_back(glm::fvec3(min.x, min.y, min.z));// 000  0
+	//bb.push_back(glm::fvec3(max.x, min.y, min.z));// 100  1
+	//bb.push_back(glm::fvec3(max.x, max.y, min.z));// 110  2
+	//bb.push_back(glm::fvec3(min.x, max.y, min.z));// 010  3
+	//bb.push_back(glm::fvec3(min.x, min.y, max.z));// 001  4
+	//bb.push_back(glm::fvec3(max.x, min.y, max.z));// 101  5
+	//bb.push_back(glm::fvec3(max.x, max.y, max.z));// 111  6
+	//bb.push_back(glm::fvec3(min.x, max.y, max.z));// 011  7
+
+	bb.push_back(glm::fvec3(min.x, min.y, min.z));
+	bb.push_back(glm::fvec3(min.x, min.y, max.z));
+	bb.push_back(glm::fvec3(min.x, max.y, min.z));
+	bb.push_back(glm::fvec3(min.x, max.y, max.z));
+	bb.push_back(glm::fvec3(max.x, max.y, max.z));
+	bb.push_back(glm::fvec3(max.x, min.y, min.z));
+	bb.push_back(glm::fvec3(max.x, min.y, max.z));
+	bb.push_back(glm::fvec3(max.x, max.y, min.z));
+
+	return bb;
+
+}
+
