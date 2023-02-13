da1347cc2a3a5a90d9cc494d97581887c5d3a62e
YusraRa <yusra.rayyan22@gmail.com>
2023-02-13 08:25:57 +0200

    Update Renderer.h
    finding the right z for each pixel. but the result did not change that much.

--- Viewer/include/Renderer.h
+++ Viewer/include/Renderer.h
@@ -39,7 +39,7 @@ public:
 
 	void Ambient(MeshModel& Mesh, glm::vec3 ambient);
 	void Diffuse(glm::vec3 light_pos, glm::vec3 ambient, glm::vec3 diffuce_color, MeshModel& Mesh);
-	void Specular(vector<vector<float>>z_depth, glm::fvec2 z_max_min, glm::fvec2 z_max_min_avg, glm::vec3 light_pos, glm::vec3 ambient,glm::vec3 diffuce_color, float Ks, float power, glm::vec3 specular_color, MeshModel& Mesh, glm::vec3 viewPos);
+	void Specular(float Fa, vector<vector<float>>z_depth, glm::fvec2 z_max_min, glm::fvec2 z_max_min_avg, glm::vec3 light_pos, glm::vec3 ambient,glm::vec3 diffuce_color, float Ks, float power, glm::vec3 specular_color, MeshModel& Mesh, glm::vec3 viewPos);
 	void Reflect(glm::vec3 light_pos, float specular, float power, glm::vec3 specular_color, MeshModel& Mesh, glm::vec3 viewPos);
 
 
