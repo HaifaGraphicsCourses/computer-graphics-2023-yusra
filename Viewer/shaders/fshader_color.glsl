ec75579e5ef5f984c7fcf85c25a4466ded6ee14f
YusraRa <yusra.rayyan22@gmail.com>
2023-02-27 22:37:49 +0200

    Update fshader_color.glsl

--- Viewer/shaders/fshader_color.glsl
+++ Viewer/shaders/fshader_color.glsl
@@ -3,26 +3,32 @@
 struct Material
 {
 	sampler2D textureMap;
-	// You can add more fields here...
-	// Such as:
-	//		1. diffuse/specular relections constants
-	//		2. specular alpha constant
-	//		3. Anything else you find relevant
-	
-	/*vec3 ambient;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;	
+};
+
+struct Light
+{
+    vec3 pos;
+   // vec3 color;
+	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
-	float shininess;*/
+	float shininess;
 };
 
 // We set this field's properties from the C++ code
 uniform Material material;
+uniform Light light;
 
 // Inputs from vertex shader (after interpolation was applied)
 in vec3 fragPos;
 in vec3 fragNormal;
 in vec2 fragTexCoords;
+
 in vec3 orig_fragPos;
+
 // The final color of the fragment (pixel)
 out vec4 frag_color;
 
@@ -30,51 +36,27 @@ void main()
 {
 	// Sample the texture-map at the UV coordinates given by 'fragTexCoords'
 	vec3 textureColor = vec3(texture(material.textureMap, fragTexCoords));
-
-	frag_color = vec4(orig_fragPos,1);
+	//frag_color = vec4(orig_fragPos,1);
 
 	// ambient
-	//vec3 ambient = lightColor * material.ambient;
+	vec3 ambient = light.ambient * material.ambient * textureColor;
+	
 	// diffuse
-	//vec3 norm = normalize(Normal);
-	//vec3 lightDir = normalize(lightPos - FragPos);
-	//float diff = max(dot(norm, lightDir), 0.0);
-	//vec3 diffuse = lightColor * (diff * material.diffuse);
+	vec3 norm = normalize(fragNormal);
+	vec3 lightDir = normalize(light.pos - fragPos);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = light.diffuse * (diff * material.diffuse) * textureColor;
+	
 	// specular
-	//vec3 viewDir = normalize(viewPos - FragPos);
-	//vec3 reflectDir = reflect(-lightDir, norm);
-	//float spec = pow(max(dot(viewDir, reflectDir), 0.0),
-	//material.shininess);
-	//vec3 specular = lightColor * (spec * material.specular);
-	//vec3 result = ambient + diffuse + specular;
-	//FragColor = vec4(result, 1.0);*/
+	vec3 viewDir = normalize(-fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), light.shininess);
+    vec3 specular = light.specular * (spec * material.specular) * textureColor;
+	
+	vec3 result = ambient + diffuse + specular;
+	frag_color = vec4(result, 1.0);
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//#version 150
-
-//in  vec2 texCoord;
-//out vec4 fColor;
-
-//uniform sampler2D texture;
-
-//void main() 
-//{ 
-//   fColor = textureLod( texture, texCoord, 0 );
-//} 
 
