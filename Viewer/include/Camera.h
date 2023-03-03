2ce65437ebff96abad84cf35aa017897cb55c317
YusraRa <yusra.rayyan22@gmail.com>
2023-03-03 10:00:29 +0200

    Update Camera.h
    fixing 6

--- Viewer/include/Camera.h
+++ Viewer/include/Camera.h
@@ -18,27 +18,22 @@ public:
 	void setupMatrix();
 
 	void SetCameraLookAt(const glm::fvec3& eye, const glm::fvec3& at, const glm::fvec3& up);
-	void SetOrthographicProjectionMatrix(float b, float t, float l, float r, float Zn, float Zf);
-	void SetPerspectiveProjectionMatrix(float fovy, float aspect, float Zn, float Zf);
+	//void SetOrthographicProjectionMatrix(float b, float t, float l, float r, float Zn, float Zf);
+	//void SetPerspectiveProjectionMatrix(float fovy, float aspect, float Zn, float Zf);
 	glm::mat4x4& GetProjectionTransformation();
 	const glm::mat4x4& GetViewTransformation();
-	void SetTransformation(const glm::mat4x4& transform);
-	void SetProjection(const glm::mat4x4& projection);
+	//void SetTransformation(const glm::mat4x4& transform);
+	//void SetProjection(const glm::mat4x4& projection);
 
 	glm::mat4 GetCAMERATranslate();
 	void SetCAMERATranslate(float t, int index);
-
 	glm::mat4 GetCAMERARotate_X();
 	void SetCAMERARotate_X(float degree);
-
 	glm::mat4 GetCAMERARotate_Y();
 	void SetCAMERARotate_Y(float degree);
-
 	glm::mat4 GetCAMERARotate_Z();
 	void SetCAMERARotate_Z(float degree);
-
 	glm::mat4 Mul_RotateMat_CAMERA();
-
 	void SetCamera_position(glm::fvec3 c_p);
 	glm::fvec3 GetCamera_position();
 
@@ -54,6 +49,8 @@ public:
 	void TransformCamera();
 	glm::mat4x4 GetCamTransformation();
 
+	void Zoom(float factor);
+
 	bool orth;
 	bool pers;
 
@@ -61,17 +58,21 @@ public:
 private:
 	glm::mat4x4 view_transformation;
 	glm::mat4x4 projection_transformation;
+	
 	glm::mat4x4	orth_mat;
 	glm::mat4x4 pers_mat;
+	
 	glm::mat4x4 cam_transformation;
 	glm::mat4 Translate_c;
 	glm::mat4 Rotate_c_x;
 	glm::mat4 Rotate_c_y;
 	glm::mat4 Rotate_c_z;
 	glm::fvec3 Camera_position;
+	
 	glm::fvec3 eye;
 	glm::fvec3 at;
 	glm::fvec3 up;
+	
 	float aspectRatio;
 	float zoom;
 	float fovy;
