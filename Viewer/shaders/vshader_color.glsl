2be3907230ea2a72be714feac88f0e7a76eb821b
YusraRa <yusra.rayyan22@gmail.com>
2023-03-03 18:07:18 +0200

    Update vshader_color.glsl
    fixing 8

--- Viewer/shaders/vshader_color.glsl
+++ Viewer/shaders/vshader_color.glsl
@@ -15,13 +15,16 @@ out vec3 fragPos;
 out vec3 fragNormal;
 out vec2 fragTexCoords;
 
+//for toon shading
+out vec3 surface_normal;
+
 void main()
 {
 	// Apply the model transformation to the 'position' and 'normal' properties of the vertex,
 	// so the interpolated values of these properties will be available for usi n the fragment shader
 	orig_fragPos = vec3(vec4(pos, 1.0f));
-	fragPos = vec3(model * vec4(pos, 1.0f));
-	fragNormal = mat3(model) * normal;
+	fragPos = vec3(view * model * vec4(pos, 1.0f));
+	fragNormal = mat3(transpose(inverse(view * model))) * normal;
 
 	// Pass the vertex texture coordinates property as it is. Its interpolated value
 	// will be avilable for us in the fragment shader
@@ -29,12 +32,9 @@ void main()
 
 	// This is an internal OpenGL variable, we must set a value to this variable
 	gl_Position = projection * view *  model * vec4(pos, 1.0f);
+
+	surface_normal = vec3(model * vec4(normal, 1.0));
 }
-
-
-
-
-
 
 
 
